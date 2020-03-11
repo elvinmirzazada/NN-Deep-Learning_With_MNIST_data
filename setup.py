fce87a661b4c8cd529bfbfdb2e04c78ae8c2fbb0
Elvin <mirzezade.elvin93@gmail.com>
2020-03-11 10:04:08 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,18 +23,10 @@ setup(
         'hack/worker-liveness.sh',
         'hack/worker-readiness.sh'
     ],
-    package_data={
-        'f8a_worker': [
-            os.path.join('dispatcher', 'migration_dir', '*.json')
-        ]
-    },
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data={},
+    packages=find_packages(exclude=['model']),
     include_package_data=True,
     install_requires=get_requirements(),
-    author='Pavel Odvody',
-    author_email='podvody@redhat.com',
-    description='fabric8-analytics workers & utilities',
-    license='GPLv3',
-    keywords='fabric8-analytics analysis worker',
-    url='https://github.com/fabric8-analytics/fabric8-analytics-worker'
+
+    url='https://github.com/elvinmirzazadeh/NN-Deep-Learning_With_MNIST_data.git'
 )
